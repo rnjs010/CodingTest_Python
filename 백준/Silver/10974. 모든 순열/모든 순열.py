0287a61a015f381f101e49420c801901d750a254
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2023-05-02 23:12:16 +0900

    [Silver III] Title: 모든 순열, Time: 116 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,5 +1,4 @@
 from itertools import permutations
 n = int(input())
-li = list(permutations(range(1, n+1), n))
-for i in sorted(li):
+for i in permutations(range(1, n+1), n):
     print(' '.join(map(str, i)))
