68a145ccc2a7843a384a5cda431d58888ddfb8ef
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2023-07-15 17:44:47 +0900

    [Silver V] Title: 호텔 방 번호, Time: 864 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.py
+++ 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.py
@@ -4,12 +4,10 @@ while True:
         ans = 0
 
         for i in range(n, m + 1):
-            li = [0] * 10
+            s = set()
             for j in str(i):
-                li[int(j)] += 1
-                if (2 in li) or (3 in li) or (4 in li):
-                    break
-            else:
+                s.add(j)
+            if len(s) == len(str(i)):
                 ans += 1
         
         print(ans)
