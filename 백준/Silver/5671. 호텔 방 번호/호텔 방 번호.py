86ef049f99fd457b18bd02c8e06cfb3b046dabe3
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2023-07-15 17:52:45 +0900

    [Silver V] Title: 호텔 방 번호, Time: 616 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.py
+++ 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.py
@@ -2,14 +2,9 @@ while True:
     try:
         n, m = map(int, input().split())
         ans = 0
-
         for i in range(n, m + 1):
-            s = set()
-            for j in str(i):
-                s.add(j)
-            if len(s) == len(str(i)):
+            if len(set(str(i))) == len(str(i)):
                 ans += 1
-        
         print(ans)
     except:
         break
