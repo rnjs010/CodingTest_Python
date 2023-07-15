c1da0c48331a9063e924385610ef4e3728f50d2e
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2023-07-16 03:05:29 +0900

    [Silver III] Title: N과 M (5), Time: 212 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,15 +1,15 @@
 n, m = map(int, input().split())
 li = sorted([*map(int, input().split())])
-used = [0] * n
+
 def perm(arr):
     if len(arr) == m:
         print(*arr)
         return
     
     for i in range(n):
-        if used[i] == 0:
-            used[i] = 1
-            perm(arr + [li[i]])
-            used[i] = 0
+        if li[i] not in arr:
+            arr.append(li[i])
+            perm(arr)
+            arr.pop()
 
 perm([])
