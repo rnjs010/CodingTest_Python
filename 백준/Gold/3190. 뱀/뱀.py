735d21ed3bf75aa7967c13f4f45758b7b4168fa6
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2023-08-13 19:49:37 +0900

    [Gold IV] Title: 뱀, Time: 68 ms, Memory: 34192 KB -BaekjoonHub

---
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n = int(input())
+k = int(input())
+graph = [[0]*n for _ in range(n)]
+for _ in range(k):
+    x, y = map(int, input().split())
+    graph[x-1][y-1] = 1
+
+l = int(input())
+li = deque([input().rstrip().split() for _ in range(l)])
+
+x = [1, 0, -1, 0]
+y = [0, 1, 0, -1]
+time, d, nx, ny = 0, 0, 0, 0
+dq = deque([(nx, ny)])
+while True:
+    if li and int(li[0][0]) == time:
+        d += (1 if li.popleft()[1] == 'D' else -1)
+
+    nx += x[d % 4]
+    ny += y[d % 4]
+    if nx < 0 or ny < 0 or nx >= n or ny >= n or (ny, nx) in dq:
+        break
+
+    dq.append((ny, nx))
+    if graph[ny][nx] == 1:
+        graph[ny][nx] = 0
+    else:
+        dq.popleft()
+
+    time += 1
+
+print(time + 1)
