9d66799dac1958428a6dcc5d5f067142d72e5b1b
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2023-08-20 22:37:10 +0900

    [Silver I] Title: 작업, Time: 340 ms, Memory: 48628 KB -BaekjoonHub

--- 백준/Silver/21937. 작업/작업.py
+++ 백준/Silver/21937. 작업/작업.py
@@ -10,17 +10,13 @@ for _ in range(m):
     graph[b].append(a)
 
 cnt = 0
-def dfs(x):
-    global cnt
-    q = deque()
-    q.append(x)
-    while q:
-        x = q.popleft()
-        for i in graph[x]:
-            if not visit[i]:
-                visit[i] = 1
-                q.append(i)
-                cnt += 1
-    return cnt
+q = deque([int(input())])
+while q:
+    x = q.popleft()
+    for i in graph[x]:
+        if not visit[i]:
+            visit[i] = 1
+            q.append(i)
+            cnt += 1
 
-print(dfs(int(input())))
+print(cnt)
