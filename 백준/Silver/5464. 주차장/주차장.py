12254b8d24f81b322da75e230d04b7804e8c0f87
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2023-11-19 14:51:20 +0900

    [Silver II] Title: 주차장, Time: 80 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/5464. 주차장/주차장.py
+++ 백준/Silver/5464. 주차장/주차장.py
@@ -5,6 +5,7 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 p = [int(input()) for _ in range(n)]
 use = [0] * n
+
 w = [0] * (m+1)
 for i in range(1, m+1):
     w[i] = int(input())
@@ -15,10 +16,9 @@ for _ in range(2*m):
     car = int(input())
     if car > 0:
         q.append(car)
-        for j in range(n):
-            if use[j] == 0:
-                use[j] = car
-                q.popleft()
+        for i in range(n):
+            if use[i] == 0:
+                use[i] = q.popleft()
                 break
     else:
         idx = use.index(-car)
