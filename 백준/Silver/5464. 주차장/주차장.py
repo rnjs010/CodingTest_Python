ce08bb026efe2b01bd818f23bcd01cadc1ab2a40
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2023-11-19 14:42:10 +0900

    [Silver II] Title: 주차장, Time: 80 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Silver/5464. 주차장/주차장.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+p = [int(input()) for _ in range(n)]
+use = [0] * n
+w = [0] * (m+1)
+for i in range(1, m+1):
+    w[i] = int(input())
+
+q = deque([])
+total = 0
+for _ in range(2*m):
+    car = int(input())
+    if car > 0:
+        q.append(car)
+        for j in range(n):
+            if use[j] == 0:
+                use[j] = car
+                q.popleft()
+                break
+    else:
+        idx = use.index(-car)
+        use[idx] = 0
+        total += (w[-car] * p[idx])
+        if q:
+            use[idx] = q.popleft()
+
+print(total)
