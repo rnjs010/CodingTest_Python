66d025b398e88e7c29b2392d2013febf53a22862
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2024-04-30 03:31:04 +0900

    [D3] Title: 세상의 모든 팰린드롬, Time: 94 ms, Memory: 43,704 KB -BaekjoonHub

--- SWEA/D3/4522. 세상의 모든 팰린드롬/세상의 모든 팰린드롬.py
+++ SWEA/D3/4522. 세상의 모든 팰린드롬/세상의 모든 팰린드롬.py
@@ -1,14 +1,13 @@
 T = int(input())
-for test_case in range(1, T + 1):
+for tc in range(1, T + 1):
     word = input()
     re_word = word[::-1]
     half = len(word) // 2
     ans = 'Exist'
-    if word != re_word:
-        for i in range(half):
-            if word[i] != re_word[i]:
-                if word[i] != '?' and re_word[i] != '?':
-                    ans = 'Not exist'
-                    break
+    for i in range(half):
+        if word[i] != re_word[i]:
+            if word[i] != '?' and re_word[i] != '?':
+                ans = 'Not exist'
+                break
 
-    print(f'#{test_case} {ans}')
+    print(f'#{tc} {ans}')
