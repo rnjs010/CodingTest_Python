5edc0554a17522cbc90231afbe5ceea080ac8768
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2024-05-01 04:22:56 +0900

    [D3] Title: 오목 판정, Time: 154 ms, Memory: 49,128 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,22 +1,21 @@
-dx, dy = [-1, -1, -1, 0, 1, 1, 1, 0], [-1, 0, 1, 1, 1, 0, -1, -1]
+dx, dy = [0, 1, 1, 1], [1, 1, 0, -1]
+def check():
+    for x in range(N):
+        for y in range(N):
+            if board[x][y] == 'o':
+                for k in range(4):
+                    for i in range(1, 5):
+                        nx, ny = x + (dx[k] * i), y + (dy[k] * i)
+                        if not (0 <= nx < N) or not (0 <= ny < N) or not (board[nx][ny] == 'o'):
+                            break
+                    else:
+                        return 'YES'
+    return 'NO'
+
 
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
     board = [input() for _ in range(N)]
-    ans = 'NO'
-    for x in range(N):
-        for y in range(N):
-            if board[x][y] == 'o':
-                for k in range(8):
-                    for i in range(1, 5):
-                        nx, ny = x + (dx[k] * i), y + (dy[k] * i)
-                        if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 'o':
-                            continue
-                        else:
-                            break
-                    else:
-                        ans = 'YES'
-                        break
-        
+    ans = check()
     print(f'#{tc} {ans}')
